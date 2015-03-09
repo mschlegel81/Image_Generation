5b52aeab14652b90f60e3e86897b2d0f029d3f39
Martin Schlegel <schlegel@itestra.de>
2015-03-09 13:43:11 +0100

    Fixed transparency handling for triangles

--- igSrc/raytrace.pas
+++ igSrc/raytrace.pas
@@ -1953,6 +1953,7 @@ FUNCTION T_FlatTriangle.rayHits(CONST ray:T_ray; CONST maxHitTime:double; OUT hi
                      +a[1]*(ray.direction[2]*by[0]-by[2]*ray.direction[0])
                      +a[2]*(ray.direction[0]*by[1]-by[0]*ray.direction[1]));
         if (x[1]>=0) and (x[2]>=0) and (x[1]+x[2]<=1) then with hitDescription do begin
+          if (material^.getTransparencyLevel(ray.start+ray.direction*hitTime)=1) and (material^.relRefractionIdx=1) then exit(false);
           result:=true;
           hitTime :=x[0];
           hitPoint:=ray.start+ray.direction*hitTime;
