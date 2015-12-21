711789405dd0aa4156dda2fdc7af5238a6fca7ef
Martin Schlegel <schlegel@itestra.de>
2015-12-22 00:42:08 +0100

    Removed catalogue GUI

--- ensureExistence.bat
+++ ensureExistence.bat
@@ -1,6 +1,5 @@
 @set fpc=c:\lazarus32\fpc\2.6.4\bin\i386-win32\fpc
 @set delp=c:\lazarus32\fpc\2.6.4\bin\i386-win32\delp
-@if "%1"=="catalogue" goto ensureCatalogue
 @if not exist ..\binary\im.exe          %fpc% -S2 -O3 -Si -XX -CX -FuigSrc -FuauxSrc im.pas -duseExtensions -o..\binary\im.exe
 @if not exist ..\binary\display.exe     %fpc% -S2 -O3 -Si -XX -CX -Sh display.pas -dexpandFileNames -FuigSrc -FuauxSrc  -o..\binary\display.exe & %delp% . ..\binary auxSrc guiSrc igSrc
 @if not exist ..\binary\expoClouds.exe  %fpc% -S2 -O3 -Si -XX -CX expoClouds.pas -ddoubleAccuracy -duseExtensions -FuigSrc -FuauxSrc -o..\binary\expoClouds.exe & %delp% . ..\binary auxSrc guiSrc igSrc
@@ -17,9 +16,5 @@
 @%delp% . ..\binary auxSrc guiSrc igSrc
 @For /f "tokens=1 delims=." %%a in ('dir /B frac_incs\frac_*.inc') do @if not exist ..\binary\%%a.exe copy frac_incs\%%a.inc frac.inc & %fpc% -S2 -O3 -Si -XX -CX fractals.pas -FuigSrc -FuauxSrc -ddoubleAccuracy -o..\binary\%%a.exe
 @%delp% . ..\binary auxSrc guiSrc igSrc
-:ensureCatalogue
-@if not exist ..\binary\catalogue.exe %fpc% guiSrc\catalogue2.lpr -S2 -Sh -CX -XX -gl -FuigSrc -FuauxSrc -FuguiSrc -FuC:\lazarus32\lcl\units\i386-win32\win32 -FuC:\lazarus32\lcl\units\i386-win32 -FuC:\lazarus32\components\lazutils\lib\i386-win32 -FuC:\lazarus32\packager\units\i386-win32 -Fu. -FuC:\lazarus32\components\synedit\units\i386-win32\win32 -dLCL -dLCLwin32 -o..\binary\catalogue.exe -WG & %delp% . ..\binary auxSrc guiSrc igSrc & del ..\binary\*.lfm
-@%delp% . ..\binary auxSrc guiSrc igSrc
-
 
 
