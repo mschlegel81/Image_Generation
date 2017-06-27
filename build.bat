e758ce978e83fcc9c827b617f067c7dd841db51b
Martin Schlegel <schlegel@itestra.de>
2017-06-27 11:24:08 +0200

    Lazarus 1.6.4

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
-..\lazarus64\fpc\3.0.0\bin\x86_64-win64\delp.exe display\lib\x86_64-win64 im\lib\x86_64-win64
+..\lazarus64\fpc\3.0.2\bin\x86_64-win64\delp.exe display\lib\x86_64-win64 im\lib\x86_64-win64
 ..\lazarus64\lazbuild.exe --bm=Release im\imageExplorer.lpi
 move im\imageExplorer.exe ..\art.bin\
 ..\lazarus64\lazbuild.exe --bm=Release display\display.lpi
 move display\imig.exe ..\art.bin\
-..\lazarus64\fpc\3.0.0\bin\x86_64-win64\delp.exe display\lib\x86_64-win64 im\lib\x86_64-win64
+..\lazarus64\fpc\3.0.2\bin\x86_64-win64\delp.exe display\lib\x86_64-win64 im\lib\x86_64-win64
