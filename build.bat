0b2c1951be3f70c673b7079ea4eac1afc90e11b4
Martin Schlegel <m.schlegel81@googlemail.com>
2020-02-04 08:50:07 +0100

    Updated build script

--- build.bat
+++ build.bat
@@ -1,7 +1,8 @@
-..\lazarus64\fpc\3.0.4\bin\x86_64-win64\delp.exe display\lib\x86_64-win64 im\lib\x86_64-win64 common igSrc
-..\lazarus64\lazbuild.exe --bm=Release im\imageExplorer.lpi
-move im\imageExplorer.exe ..\art.bin\
-..\lazarus64\lazbuild.exe --bm=Release display\display.lpi
+..\lazarus64\fpc\3.0.4\bin\x86_64-win64\delp.exe display\lib\x86_64-win64 im\lib\x86_64-win64 common igSrc > build.log 2>&1
+..\lazarus64\lazbuild.exe --bm=Release im\imageExplorer.lpi >> build.log 2>&1
+move im\imageExplorer.exe ..\art.bin\ 
+..\lazarus64\lazbuild.exe --bm=Release display\display.lpi >> build.log 2>&1
 copy display\imig.exe c:\bin\
 move display\imig.exe ..\art.bin\
-..\lazarus64\fpc\3.0.4\bin\x86_64-win64\delp.exe display\lib\x86_64-win64 im\lib\x86_64-win64 common igSrc
+..\lazarus64\fpc\3.0.4\bin\x86_64-win64\delp.exe display\lib\x86_64-win64 im\lib\x86_64-win64 common igSrc >> build.log 2>&1
+type build.log
